a30d920b22b2abbcc0351545346689df6e2065f0
exolun <mike@mikelikescoding.com>
2026-05-16 23:54:48 -0700

    Add extra Feedster sprite clearance

--- games/feedster/sw.js
+++ games/feedster/sw.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE = "feedster-shell-v8";
-const RUNTIME_CACHE = "feedster-runtime-v8";
+const SHELL_CACHE = "feedster-shell-v9";
+const RUNTIME_CACHE = "feedster-runtime-v9";
 const OFFLINE_URL = "./index.html";
 
 const PRECACHE_URLS = [
